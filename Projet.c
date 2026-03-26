27af82c0369dc776a7068abdabc73fe97e06ce28
Jason1999b <jason.bisiaux@oteria.fr>
2026-03-26 20:22:28 +0100

    fonction dédiée write_png

--- Projet.c
+++ Projet.c
@@ -3,7 +3,6 @@
 #include <string.h>
 #include <png.h>
 
-// 0-85: BLEU (texte ASCII), 86-170: VERT (données moyennes), 171-255: ROUGE (binaire pur)
 void byte_to_color(unsigned char byte, unsigned char *r, unsigned char *g, unsigned char *b) {
     if (byte <= 85) {
         *r = 0;
@@ -43,7 +42,6 @@ unsigned char *read_binary_file(const char *filename, size_t *size) {
     return data;
 }
 
-// Retourne -1 si trop grand, 0 sinon
 int calculate_dimensions(size_t size, int *width, int *height) {
     *width = 1;
     while ((*width) * (*width) < (int)size) {
@@ -65,6 +63,57 @@ const char *extract_filename(const char *path) {
         name = strrchr(path, '\\');  // Windows
     }
     return name ? name + 1 : path;
+}
+
+int write_png(const char *filename, unsigned char *data, size_t size, int width, int height) {
+    FILE *fp = fopen(filename, "wb");
+    if (!fp) {
+        return -1;
+    }
+
+    png_structp png = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+    if (!png) {
+        fclose(fp);
+        return -1;
+    }
+
+    png_infop info = png_create_info_struct(png);
+    if (!info) {
+        png_destroy_write_struct(&png, NULL);
+        fclose(fp);
+        return -1;
+    }
+
+    png_init_io(png, fp);
+    png_set_IHDR(png, info, width, height, 8, PNG_COLOR_TYPE_RGB,
+                 PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
+    png_write_info(png, info);
+
+    unsigned char *row = malloc(width * 3);
+    if (!row) {
+        png_destroy_write_struct(&png, &info);
+        fclose(fp);
+        return -1;
+    }
+
+    size_t idx = 0;
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            unsigned char val = (idx < size) ? data[idx++] : 0;
+            unsigned char r, g, b;
+            byte_to_color(val, &r, &g, &b);
+            row[x * 3 + 0] = r;
+            row[x * 3 + 1] = g;
+            row[x * 3 + 2] = b;
+        }
+        png_write_row(png, row);
+    }
+
+    png_write_end(png, NULL);
+    png_destroy_write_struct(&png, &info);
+    fclose(fp);
+    free(row);
+    return 0;
 }
 
 int main(int argc, char *argv[]) {
@@ -112,68 +161,15 @@ int main(int argc, char *argv[]) {
     }
     snprintf(output_name, output_name_len, "%s.png", input_name);
 
-    FILE *fp = fopen(output_name, "wb");
-    if (!fp) {
+    if (write_png(output_name, data, size, width, height) != 0) {
         printf("Error: cannot create %s\n", output_name);
         free(output_name);
         free(data);
         return 1;
     }
 
-    png_structp png = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
-    if (!png) {
-        printf("Error: PNG creation failed\n");
-        fclose(fp);
-        free(output_name);
-        free(data);
-        return 1;
-    }
-
-    png_infop info = png_create_info_struct(png);
-    if (!info) {
-        printf("Error: PNG info creation failed\n");
-        png_destroy_write_struct(&png, NULL);
-        fclose(fp);
-        free(output_name);
-        free(data);
-        return 1;
-    }
-
-    png_init_io(png, fp);
-    png_set_IHDR(png, info, width, height, 8, PNG_COLOR_TYPE_RGB,
-                 PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
-    png_write_info(png, info);
-
-    unsigned char *row = malloc(width * 3);
-    if (!row) {
-        printf("Error: not enough memory for row\n");
-        png_destroy_write_struct(&png, &info);
-        fclose(fp);
-        free(output_name);
-        free(data);
-        return 1;
-    }
-
-    size_t idx = 0;
-    for (int y = 0; y < height; y++) {
-        for (int x = 0; x < width; x++) {
-            unsigned char val = (idx < size) ? data[idx++] : 0;
-            unsigned char r, g, b;
-            byte_to_color(val, &r, &g, &b);
-            row[x * 3 + 0] = r;
-            row[x * 3 + 1] = g;
-            row[x * 3 + 2] = b;
-        }
-        png_write_row(png, row);
-    }
-
-    png_write_end(png, NULL);
-    png_destroy_write_struct(&png, &info);
-    fclose(fp);
-    free(row);
-    free(data);
-
     printf("Image created: %s\n", output_name);
     free(output_name);
+    free(data);
     return 0;
 }
